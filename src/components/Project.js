5939e2555d7d2f0380483282112f25ba7fa97602
Diego <diegoyon@outlook.com>
2022-12-16 15:06:05 -0600

    style each project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import './Project.css';
+import { AiFillFolder } from 'react-icons/ai';
+import { FiGithub, FiExternalLink } from 'react-icons/fi';
 
 const Project = (props) => {
   const { title, gitHub, live, description, skills } = props;
   return (
     <div className="project-card">
-      <p>{title}</p>
-      <p>{gitHub}</p>
-      <p>{live}</p>
-      <p>{description}</p>
-      <p>{skills}</p>
+      <div className="header-icons">
+        <AiFillFolder className="project-icons-left" />
+        <div className="project-icons-right">
+          <a href={gitHub}>
+            <FiGithub />
+          </a>
+          <a href={live}>
+            <FiExternalLink />
+          </a>
+        </div>
+      </div>
+      <div className="project-details">
+        <h4>{title}</h4>
+        <p>{description}</p>
+        <div className="project-skills">
+          {skills.map((skill) => (
+            <p>{skill}</p>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/ProjectsData.js
+++ src/components/ProjectsData.js
@@ -20,6 +20,27 @@ const ProjectsData = [
     description: 'A great description',
     skills: ['html', 'css', 'rails'],
   },
+  {
+    title: '4th project',
+    gitHub: 'www.google.com',
+    live: 'www.gmail.com',
+    description: 'A great description',
+    skills: ['html', 'css', 'rails'],
+  },
+  {
+    title: '5th project',
+    gitHub: 'www.google.com',
+    live: 'www.gmail.com',
+    description: 'A great description',
+    skills: ['html', 'css', 'rails'],
+  },
+  {
+    title: '6th project',
+    gitHub: 'www.google.com',
+    live: 'www.gmail.com',
+    description: 'A great description',
+    skills: ['html', 'css', 'rails'],
+  },
 ];
 
 export default ProjectsData;
