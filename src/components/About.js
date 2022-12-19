41cb4f0376c46953b6a1fcbaca2224025f8f81b1
Diego <diegoyon@outlook.com>
2022-12-19 15:25:45 -0600

    edit about

--- src/components/About.js
+++ src/components/About.js
@@ -6,17 +6,18 @@ const About = () => {
     <section className="about" id="about">
       <h2>About me</h2>
       <p>
-        Hello! My name is Diego and I enjoy creating things that live on the
-        internet. My interest in web development started back in 2018 when I
-        decided to try out for my first school project — turns out putting
-        together a simple website taught me a lot about HTML & CSS!
+        Hello! My name is Diego and I enjoy building new things that live on the
+        internet. My interest in computer programming started back in 2010 when
+        I was in college studying Mechatronics Engineering. Now, I enjoy
+        building full web applications while solving all kind of different
+        challenges that they may present.
       </p>
       <p>
-        I’ve had the privilege of working with some experienced professionals
-        which has enhanced my skills and rate of learning. My main focus these
-        days is building accessible, inclusive products and digital experiences,
-        as well as creating designs and illustrations. With my experience I have
-        what it takes to solve real world problems
+        Lately, I’ve had the privilege of working remotely with other software
+        developers around the world which has enhanced my skills and rate of
+        learning. My main focus these days is building digital products with
+        great user experience and unique designs. With my experience I have what
+        it takes to solve real world problems.
       </p>
       <button>Get My Resume</button>
     </section>
