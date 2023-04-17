2bd6d1a25989d183b2bc87e96ace8b4addbe4880
Diego <diegoyon@outlook.com>
2023-04-17 07:50:59 -0600

    fix budgetapp live link

--- src/components/Project/ProjectsData.js
+++ src/components/Project/ProjectsData.js
@@ -8,7 +8,7 @@ const ProjectsData = [
   {
     title: 'BudgetApp',
     gitHub: 'https://github.com/diegoyon/BudgetApp',
-    live: 'https://budget-app-md2l.onrender.com/',
+    live: 'https://budget-app-x410.onrender.com/',
     description: [
       'BudgetApp is a mobile web application where you can manage your budget: you have a list of transactions associated with a category, so that you can see how much money you spent and on what.',
     ],
